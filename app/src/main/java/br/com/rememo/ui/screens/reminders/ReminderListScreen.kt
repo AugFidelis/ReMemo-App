bd5ed1630c38a0fb119b0dbfd658352ffd5ec885
AugFidelis <augustofidelis2003@gmail.com>
2026-08-28 11:17:22 -0300

    28/08 - Adição dos cards na tela de lembretes
    - Adicionada a base visual dos cards que mostram o nome, data/horário do lembrete, o tempo de repetição e os alarmes vinculados ao lembrete, faltando apenas o checkbox para marcar/desmarcar lembrete como concluído

--- app/src/main/java/br/com/rememo/ui/screens/reminders/ReminderListScreen.kt
+++ app/src/main/java/br/com/rememo/ui/screens/reminders/ReminderListScreen.kt
@@ -2,8 +2,10 @@ package br.com.rememo.ui.screens.reminders
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
@@ -11,12 +13,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.darkColorScheme
@@ -29,8 +38,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.rememo.R
 import br.com.rememo.ui.components.ReMemoTopBar
+
+data class Reminder(
+    val title: String,
+    val dateTime: String,
+    val repeatInterval: String,
+    val alarms: List<String>,
+    val completed: Boolean
+)
 
 @Composable
 fun ReminderListScreen(modifier: Modifier = Modifier) {
@@ -65,15 +85,21 @@ fun ReminderListScreen(modifier: Modifier = Modifier) {
                     Text("Ativos")
 
                     Spacer(modifier = Modifier.weight(1f))
-                    
-                    if(selectedTab == 0){
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(2.dp)
-                                .background(Color(0xFFFFFFFF))
-                        )
-                    }
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(2.dp)
+                            .background(
+                                if(selectedTab == 0){
+                                    Color.White
+                                }
+                                else{
+                                    Color.Transparent
+                                }
+                            )
+                    )
+
                 }
 
                 Column(
@@ -92,41 +118,166 @@ fun ReminderListScreen(modifier: Modifier = Modifier) {
 
                     Spacer(modifier = Modifier.weight(1f))
 
-                    if(selectedTab == 1){
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(2.dp)
-                                .background(Color(0xFFFFFFFF))
-                        )
-                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(2.dp)
+                            .background(
+                                if(selectedTab == 1){
+                                    Color.White
+                                }
+                                else{
+                                    Color.Transparent
+                                }
+                            )
+                    )
                 }
-
-
-//                TextButton(
-//                    onClick = {
-//                        selectedTab = 0
-//                    },
-//                    modifier = Modifier.weight(1f)
-//                ) {
-//                    Text("Ativos")
-//                }
-//
-//                TextButton(
-//                    onClick = {
-//                        selectedTab = 1
-//                    },
-//                    modifier = Modifier.weight(1f)
-//                ) {
-//                    Text("Concluídos")
-//                }
             }
 
-            if(selectedTab == 0){
-                Text("Ativos selecionado")
+//            ---------------------------------------------------------------------------
+
+//            if(selectedTab == 0){
+//                Text("Ativos selecionado")
+//            }
+//            else if(selectedTab == 1){
+//                Text("Concluídos selecionado")
+//            }
+
+            val reminders = remember {
+                mutableListOf(
+                    Reminder(
+                        title = "Lavar roupas",
+                        dateTime = "Seg., 17 de ago., 16:00",
+                        repeatInterval = "30 min",
+                        alarms = listOf(
+                            "Seg., 17 de ago., 15:00",
+                            "Seg., 17 de ago., 15:30",
+                            "Seg., 17 de ago., 15:45"
+                        ),
+                        completed = false
+                    ),
+
+                    Reminder(
+                        title = "Estudar Kotlin",
+                        dateTime = "Ter., 18 de ago., 19:00",
+                        repeatInterval = "1 hora",
+                        alarms = listOf(
+                            "Ter., 18 de ago., 18:00",
+                            "Ter., 18 de ago., 18:30"
+                        ),
+                        completed = false
+                    ),
+
+                    Reminder(
+                        title = "Comprar presente",
+                        dateTime = "Qua., 19 de ago., 14:00",
+                        repeatInterval = "30 min",
+                        alarms = listOf(
+                            "Qua., 19 de ago., 13:00"
+                        ),
+                        completed = true
+                    )
+                )
             }
-            else if(selectedTab == 1){
-                Text("Concluídos selecionado")
+
+            val filteredReminders = reminders.filter {
+                it.completed == (selectedTab == 1)
+            }
+
+            LazyColumn(
+                contentPadding = PaddingValues(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp,
+                    bottom = 90.dp
+                ),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+            ){
+                items(filteredReminders){ reminder ->
+
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(16.dp)
+                        ) {
+                            Text(
+                                text = reminder.title,
+                                fontSize = 22.sp
+                            )
+
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.padding(top = 4.dp)
+                            ) {
+                                Surface(
+                                    shape = RoundedCornerShape(50.dp),
+                                    color = MaterialTheme.colorScheme.primaryContainer
+                                ) {
+                                    Row(
+                                        modifier = Modifier.padding(
+                                            horizontal = 8.dp,
+                                            vertical = 2.dp
+                                        ),
+                                        verticalAlignment = Alignment.CenterVertically
+                                    ) {
+                                        Icon(
+                                            painter = painterResource(R.drawable.calendar_check_icon),
+                                            contentDescription = "Ícone de lembrete",
+                                            modifier = Modifier
+                                                .size(16.dp)
+                                        )
+
+                                        Text(
+                                            text = reminder.dateTime,
+                                            fontSize = 14.sp,
+                                            modifier = Modifier.padding(start = 4.dp)
+                                        )
+                                    }
+                                }
+
+                                Spacer(modifier = Modifier.width(8.dp))
+
+                                Icon(
+                                    painter = painterResource((R.drawable.reminder_repeat)),
+                                    contentDescription = "Ícone de repetir lembrete",
+                                    modifier = Modifier.size(16.dp)
+                                )
+
+
+                                Text(
+                                    text = reminder.repeatInterval,
+                                    fontSize = 14.sp,
+                                    modifier = Modifier.padding(start = 2.dp)
+                                )
+                            }
+
+                            reminder.alarms.forEach { alarm ->
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier.padding(start = 8.dp)
+                                ) {
+                                    Icon(
+                                        painter = painterResource(R.drawable.alarm_icon),
+                                        contentDescription = "Ícone de alarme",
+                                        modifier = Modifier
+                                            .size(16.dp)
+                                    )
+
+                                    Text(
+                                        text = alarm,
+                                        fontSize = 14.sp,
+                                        modifier = Modifier.padding(start = 4.dp)
+                                    )
+                                }
+
+                                //Spacer(modifier = Modifier.height(4.dp))
+                            }
+                        }
+                    }
+
+                }
             }
         }
     }
